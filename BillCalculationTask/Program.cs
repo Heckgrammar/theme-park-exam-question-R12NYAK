c8b48dcac472c2e7327898f48a79a8a1f7433fed
Rainyak <144434306+R12NYAK@users.noreply.github.com>
2025-03-03 22:09:22 +0000

    Update Program.cs

--- BillCalculationTask/Program.cs
+++ BillCalculationTask/Program.cs
@@ -1,25 +1,40 @@
 ﻿namespace ThemeParkCalculationTask
 {
-    internal class Program
+   internal class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            /* A theme park charges £15 per person for a daily ticket. If there are six or more people in a group, the group is given a £5 discount.
-               Write a C# program to calculate the total charge for a group of people visiting the theme park.
-               The program must:
-                • get the user to enter the number of people in a group
-                • calculate the total charge by:
-                    ○ charging £15 per person
-                    ○ reducing the total charge by £5 if there are six or more people
-                • output the total charge.
-            You should use meaningful variable name(s) inyour answer.
-            */
-            // Write your program and test it with the following test data
-            // 1: 4 people
-            // 2: 10 people
-            // 3: 6 people
-            // Commit your changes and add screnshot evidence of your program running to the Readme
+        Console.WriteLine("enter the number of people");
+        string input = Console.ReadLine();
 
-        }
+        { 
+            int numOfPeople = Convert.ToInt32(input);
+
+            if (numOfPeople > 0)
+            {
+                int pricePerCustomer = 15;
+                int discountMinimum = 6;
+                int discountAmount = 5;
+
+                int totalcharge = numOfPeople * pricePerCustomer;
+
+
+                if (numOfPeople >= discountMinimum) 
+                {
+                    totalcharge = numOfPeople*pricePerCustomer - discountAmount;
+                }
+                Console.WriteLine("The total charge for your group is £" + totalcharge);
+
+            }
+            
+                else 
+            {
+                Console.WriteLine("enter a valid number please");
+            }
+            }
+
+    }   
+
+
     }
 }
